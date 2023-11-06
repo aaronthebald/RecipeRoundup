0ac3a8a7511b529c235999d6fe74ec764059baad
Aaron Wilson <124102001+aaronthebald@users.noreply.github.com>
2023-11-06 17:51:30 -0500

    Initial Commit

---
+++ Cake/CakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CakeApp.swift
+//  Cake
+//
+//  Created by Aaron Wilson on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
