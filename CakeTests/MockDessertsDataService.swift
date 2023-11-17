8046cfcc9fff70cdea4680025878b9684e30a617
Aaron Wilson <124102001+aaronthebald@users.noreply.github.com>
2023-11-17 07:30:17 -0500

    Added testing target

---
+++ CakeTests/MockDessertsDataService.swift
@@ -0,0 +1,8 @@
+//
+//  MockDessertsDataService.swift
+//  CakeTests
+//
+//  Created by Aaron Wilson on 11/16/23.
+//
+
+import Foundation
---
+++ CakeTests/Mockable.swift
@@ -0,0 +1,8 @@
+//
+//  Mockable.swift
+//  CakeTests
+//
+//  Created by Aaron Wilson on 11/16/23.
+//
+
+import Foundation
